5bc448e56411c6780b00573a0c9043745d07472b
Yejin <110469361+yejeeni@users.noreply.github.com>
2025-12-07 22:27:36 +0900

    Simplify output using GCD for Jerry and Tom problem
    Refactor code to simplify output using GCD.

--- 백준/Bronze/16430. 제리와 톰/제리와 톰.py
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.py
@@ -1,3 +1,10 @@
-a,b = map(int, input().split()) # 1kg 의 a/b 만큼 훔쳐감
+import math
 
-print(b-a, b)
+a,b = map(int, input().split()) # 1kg 의 a/b 만큼 훔쳐감
+# print(b-a, b)
+
+p = b - a
+q = b
+g = math.gcd(p, q) # p와 q의 최대공약수 구하기
+
+print(p//g, q//g)
