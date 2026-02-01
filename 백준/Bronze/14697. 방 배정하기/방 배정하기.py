cc43a1871269ccc2fa7b26c61d2d656ab7f4e3a5
Yejin <110469361+yejeeni@users.noreply.github.com>
2026-02-01 11:00:02 +0900

    [Bronze II] Title: 방 배정하기, Time: 76 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub
    Refactor room assignment logic to optimize loops for checking combinations.

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,9 +1,9 @@
 a, b, c, n = map(int, input().split()) # a, b, c = 방의 정원 / n = 인원수
 
 is_able = False
-for i in range(a):
-    for j in range(b):
-        for k in range(c):
+for i in range(n // a + 1): # 각 방의 개수는 최대 n // 정원 까지
+    for j in range(n // b + 1):
+        for k in range(n // c + 1):
             total = a * i + b * j + c * k
 
             if total == n:
